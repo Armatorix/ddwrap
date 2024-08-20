2bfdcf7d3646d0dc3ce521954754ca89ac560474
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2024-08-20 22:40:13 +0200

    base version without imports

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
+	"embed"
+	_ "embed"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"html/template"
 	"os"
+	"strings"
 )
 
 type Method struct {
-	Name string
-	Args []string
-	Rets []string
+	Name       string
+	ContextArg *int
+	Args       []string
+	Rets       []string
 }
 type Interface struct {
 	Name    string
@@ -23,7 +29,53 @@ type TemplateValues struct {
 	Interfaces  []Interface
 }
 
+//go:embed tmpl/*
+var tmpls embed.FS
+
 func main() {
+	templates, err := template.New("").
+		Funcs(template.FuncMap{
+			"contextArg": func(m Method) string {
+				if m.ContextArg != nil {
+					return fmt.Sprintf("in%d", *m.ContextArg)
+				}
+				return "context.Background()"
+			},
+			"contextArgNameOnly": func(m Method) string {
+				if m.ContextArg != nil {
+					return fmt.Sprintf("in%d", *m.ContextArg)
+				}
+				return "_"
+			},
+			"methodArgs": func(m Method) string {
+				vals := make([]string, 0)
+				for i, arg := range m.Args {
+					vals = append(vals, fmt.Sprintf("in%d %s", i, arg))
+				}
+				return strings.Join(vals, ", ")
+			},
+			"methodArgsNamesOnly": func(m Method) string {
+				vals := make([]string, 0)
+				for i := range m.Args {
+					vals = append(vals, fmt.Sprintf("in%d", i))
+				}
+				return strings.Join(vals, ", ")
+			},
+			"methodRetsTypesOnly": func(m Method) string {
+				return strings.Join(m.Rets, ", ")
+			},
+			"methodRetsNamesOnly": func(m Method) string {
+				vals := make([]string, 0)
+				for i := range m.Rets {
+					vals = append(vals, fmt.Sprintf("out%d", i))
+				}
+				return strings.Join(vals, ", ")
+			},
+		}).
+		ParseFS(tmpls, "tmpl/*")
+	if err != nil {
+		panic(err)
+	}
 	filepath := "example/example.go"
 
 	rawFile, err := os.ReadFile(filepath)
@@ -63,6 +115,15 @@ func main() {
 									Rets: make([]string, 0),
 								}
 								if funcType, ok := field.Type.(*ast.FuncType); ok {
+									var ctxArg *int
+									for i, param := range funcType.Params.List {
+										if _, ok := param.Type.(context.Context); ok {
+											ctxArg = &i
+											break
+										}
+									}
+									nextMethod.ContextArg = ctxArg
+
 									for _, param := range funcType.Params.List {
 										nextMethod.Args = append(nextMethod.Args, string(rawFile[param.Type.Pos()-1:param.Type.End()-1]))
 									}
@@ -82,5 +143,6 @@ func main() {
 			return true
 		})
 	}
-	fmt.Println(templateValues)
+
+	fmt.Println(templates.ExecuteTemplate(os.Stdout, "dd.tmpl", templateValues))
 }
